254a4948ccc521a79bccb25772df8a367589b1ca
JawadZ02 <114508855+JawadZ02@users.noreply.github.com>
2025-03-16 13:50:59 +0400

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -1,7 +1,6 @@
-# Downloads
-curl -s -o login.sh -L "https://raw.githubusercontent.com/JawadZ02/MacRDP/main/mac/login.sh"
+#!/bin/bash
 
-# Disable spotlight indexing
+# Disable Spotlight indexing
 sudo mdutil -i off -a
 
 # Create new account
@@ -10,8 +9,7 @@ sudo dscl . -create /Users/runneradmin UserShell /bin/bash
 sudo dscl . -create /Users/runneradmin RealName Runner_Admin
 sudo dscl . -create /Users/runneradmin UniqueID 1001
 sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
-sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/runneradmin
 sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
@@ -21,12 +19,13 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-# Start VNC/reset changes
+# Start VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-# Install cloudflared
-brew install cloudflare/cloudflare/cloudflared
+# Install Pinggy
+brew install openssh
 
-# Configure cloudflared and start it
-cloudflared tunnel --url http://localhost:5900 --no-autoupdate --legacy-dns --logfile /tmp/cloudflared.log &
+# Start Pinggy TCP tunnel
+nohup ssh -p 443 -R 0:localhost:5900 a.pinggy.io > pinggy.txt 2>&1 &
+sleep 10  # Give Pinggy some time to establish the tunnel
